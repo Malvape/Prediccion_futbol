4f1ad44e589848d4489a6c721da6aff428654ecc
malvape <maalq0105@gmail.com>
2024-05-25 21:39:15 -0500

    devcontainer

---
+++ .devcontainer/setup-kaggle.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Create the .kaggle directory
+mkdir -p /home/vscode/.kaggle
+
+# Add the kaggle.json file with your credentials
+cat <<EOF > /home/vscode/.kaggle/kaggle.json
+{
+  "username": "mauricioalvarezq",
+  "key": "0ef9c395468b646e37543e131fd03cb4"
+}
+EOF
+
+# Set permissions
+chmod 600 /home/vscode/.kaggle/kaggle.json
+mkdir -p /home/vscode/datasets
+
+# Download the dataset
+kaggle datasets download -d martj42/international-football-results-from-1872-to-2017 -p /home/vscode/datasets
+
+# Unzip the dataset
+unzip /home/vscode/datasets/archive.zip -d /home/vscode/datasets
